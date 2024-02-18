42cfab64527416c9bd08141ed787c80ae8efd864
aofserver <mr.sarawutnacwijit@gmail.com>
2024-02-18 17:19:50 +0700

    update

--- .pio/build/esp32dev/lib836/xxtea-iot-crypt/xxtea_core.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/lib836/xxtea-iot-crypt/xxtea_core.c.o: \
- .pio/libdeps/esp32dev/xxtea-iot-crypt/src/xxtea_core.c \
- .pio/libdeps/esp32dev/xxtea-iot-crypt/src/xxtea_core.h
--- .pio/build/esp32dev/lib9b7/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib9b7/WebServer/detail/mimetable.cpp.o: \
- /Users/sarawutnacwijit/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/sarawutnacwijit/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/sarawutnacwijit/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
